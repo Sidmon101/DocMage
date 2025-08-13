6bff0bf35ce5acb2048e484fcbf028518fd2964e
Sidmon101 <64657186+Sidmon101@users.noreply.github.com>
2025-08-13 13:07:43 +0530

    Update settings.py

--- smart_doc_analyzer/settings.py
+++ smart_doc_analyzer/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-   .middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'smart_doc_analyzer.urls'
